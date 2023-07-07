e2810592615720f703fdfa8ecd857f6ee9154ad9
mmagician <marcin.gorny.94@protonmail.com>
2023-07-07 08:29:01 +0200

    bring back the tests

--- src/gkr/mod.rs
+++ src/gkr/mod.rs
@@ -140,3 +140,62 @@ impl<F: PrimeField, const d: usize> UniformCircuit<F, d> {
 /* Sanity checks
     - wiring indices in correct range
 */
+
+mod test {
+
+    use super::*;
+    use ark_bls12_381::Fq;
+
+    #[test]
+    fn simple_circuit() {
+        // example from Thaler's book p. 60, bottom - one gate changed for addition
+
+        // mul, layer 0 (output)
+        let mul0_0 = Wiring::new(0, 0, 1);
+        let mul0_1 = Wiring::new(1, 2, 3);
+
+        let layer_0 = Layer::new(Vec::new(), vec![mul0_0, mul0_1]);
+
+        // add, layer 0
+        // empty
+
+        // mul, layer 1
+        let mul1_0 = Wiring::new(0, 0, 0);
+        let mul1_1 = Wiring::new(1, 1, 1);
+        let mul1_2 = Wiring::new(2, 1, 2);
+
+        // add, layer 1
+        let add1_3 = Wiring::new(3, 3, 3);
+
+        let layer_1 = Layer::new(vec![add1_3], vec![mul1_0, mul1_1, mul1_2]);
+
+        let circuit = UniformCircuit::<Fq>::new(vec![layer_0, layer_1]);
+
+        let computed_out = circuit.evaluate(
+            vec![3, 2, 3, 1]
+                .iter()
+                .map(|x| Fq::from(*x as u64))
+                .collect(),
+        );
+
+        assert_eq!(
+            computed_out[0],
+            vec![36, 12]
+                .iter()
+                .map(|x| Fq::from(*x as u64))
+                .collect::<Vec<Fq>>()
+        );
+
+        println!("{:?}", computed_out);
+    }
+
+    #[test]
+    fn layer_to_mles() {
+        let mul0_0 = Wiring::new(0, 0, 1);
+        let mul0_1 = Wiring::new(1, 2, 3);
+
+        let layer_0 = Layer::new(Vec::new(), vec![mul0_0, mul0_1]);
+
+        let mles: [SparseMultilinearExtension<Fq>; 2] = layer_0.into();
+    }
+}
